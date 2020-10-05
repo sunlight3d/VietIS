07e08921eba1f181ce052c2bc38539fc0b16fda8
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-10-05 11:44:26 +0700

    update

---
+++ KhoaHocAndroidJavaT09-2020/nodejsServer/constants/constants.js
@@ -0,0 +1,3 @@
+const MAXMUM_FILE_SIZE = 50
+const PORT = 3000
+module.exports = {MAXMUM_FILE_SIZE, PORT}
--- KhoaHocAndroidJavaT09-2020/nodejsServer/routings/User.js
+++ KhoaHocAndroidJavaT09-2020/nodejsServer/routings/User.js
@@ -1,9 +1,29 @@
   
 var express = require('express')
 var router = express.Router()
-//http://users/register
+//http://192.168.1.142:3000/users/register
 router.post('/register', async (req, res) => {    
-    
+    res.json({
+        result: 'ok',
+        data: 'haha, chua co du lieu dau'
+    })
 })
 
+
 module.exports = router
+/*
+CREATE TABLE IF NOT EXISTS User (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(300) NOT NULL ,
+    name VARCHAR(300) NOT NULL ,
+    hashedPassword VARCHAR(500) NOT NULL ,
+    tokenKey VARCHAR(300) DEFAULT '',
+    expiredDate Date,
+    phoneNumber VARCHAR(100) DEFAULT '',
+    userType enum('default','facebook', 'google') default 'default'
+);
+
+INSERT INTO User(email, name, hashedPassword,tokenKey,phoneNumber, userType)
+VALUES('hoang@gmail.com', 'hoangaa', 'reijrtie', 'ttookkee', '233445', 'facebook');
+
+*/
